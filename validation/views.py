c625631d6f729e520f045cd701a93346f35bff23
Prasun Shiwakoit <champgod12@gmail.com>
2020-08-28 16:40:56 +0545

    Updated files

--- validation/views.py
+++ validation/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+import time
+
+message_obj_list = []
 
 
 def home(request):
-    print(request.method)
-    return render(request, 'index.html')
-
-
-def validate(request):
     if request.method == "POST":
-        pass
-    else:
-        redirect("/")
+        name = request.POST["name"]
+        email = request.POST["email"]
+        message = request.POST["message"]
+        print(name, email, message)
+        if not message == "offensive text":
+            message_obj = {"name": name, "email": email, "message": message}
+            message_obj_list.append(message_obj)
+        else:
+            messages.error(
+                request, f"Your post was detected as offensive and has been removed by our system")
+            redirect("/")
+    return render(request, 'index.html', context={"posts": message_obj_list})
